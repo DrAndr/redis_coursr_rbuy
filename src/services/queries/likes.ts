fc8dd8614b05a36fad12a73d36498b54896521f9
YAndrey <nextdz@gmail.com>
2025-08-23 12:27:37 +0300

    S-10 added handlers for the likes flow

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,9 +1,38 @@
-export const userLikesItem = async (itemId: string, userId: string) => {};
+import { client } from '$services/redis';
+import { itemKey, usersLikesKey } from '$services/keys';
+import isEmpty from 'lodash/isEmpty';
+import { getItems } from '$services/queries/items';
 
-export const likedItems = async (userId: string) => {};
+export const userLikesItem = async (itemId: string, userId: string) => {
+	return await client.sIsMember(usersLikesKey(userId), itemId);
+};
 
-export const likeItem = async (itemId: string, userId: string) => {};
+export const likedItems = async (userId: string) => {
+	const likedItemsIds = await client.sMembers(usersLikesKey(userId));
 
-export const unlikeItem = async (itemId: string, userId: string) => {};
+	if(!isEmpty(likedItemsIds))
+		return await getItems(likedItemsIds)
 
-export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
+
+	return null;
+};
+
+export const likeItem = async (itemId: string, userId: string) => {
+	const result = await client.sAdd(usersLikesKey(userId), itemId);
+	if(result) await client.hIncrBy(itemKey(itemId), 'likes', 1);
+
+	return result;
+};
+
+export const unlikeItem = async (itemId: string, userId: string) => {
+	const result = await client.sRem(usersLikesKey(userId), itemId);
+	if (result) await client.hIncrBy(itemKey(itemId), 'likes', -1);
+
+	return result;
+};
+
+export const commonLikedItems = async (userOneId: string, userTwoId: string) => {
+	const intersectedIds = await client.sInter([usersLikesKey(userOneId), usersLikesKey(userTwoId)]);
+
+	return await getItems(intersectedIds);
+};
