13d5c121264dfc1f9c8ee86adee4f046802c328c
YAndrey <nextdz@gmail.com>
2025-08-31 02:29:40 +0300

    S-17 the incrementView has been rewrited with LUA script usage, for prewint issue with requests concurency

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,13 +1,32 @@
 import { client } from '$services/redis';
-import { itemKey, itemsByViewsKey, itemsViewsKey } from '$services/keys';
+// import { itemKey, itemsByViewsKey, itemsViewsKey } from '$services/keys';
 
 export const incrementView = async (itemId: string, userId: string) => {
-	const isViewUniq = await client.pfAdd(itemsViewsKey(itemId), userId);
 
-	if(!isViewUniq) return null; // view already counted for this user
+	// LUA script that defined at src/services/redis/client.ts
+	return await client.incrementViews(itemId, userId);
 
-	return await Promise.all([
-		client.hIncrBy(itemKey(itemId), 'views',1),
-		client.zIncrBy(itemsByViewsKey(), 1, itemId)
-	])
+	/**
+	 * Old approach:
+	 * A potential problem is that with a large number of
+	 * views between receiving and updating requests,
+	 * there may be additional requests/views that will not be counted.
+	 */
+	// const isViewUniq = await client.pfAdd(itemsViewsKey(itemId), userId);
+	//
+	// if(!isViewUniq) return null; // view already counted for this user
+	//
+	// return await Promise.all([
+	// 	client.hIncrBy(itemKey(itemId), 'views',1),
+	// 	client.zIncrBy(itemsByViewsKey(), 1, itemId)
+	// ]);
 };
+
+// LUA script preparing:
+// Keys I need to access KEYS:
+//    1) itemsViewsKey
+//    2) itemKey
+//    3) itemsByViewsKey
+// Arguments ARGV:
+//    1) itemId
+//    2) userId
--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,12 +1,64 @@
-import { createClient } from 'redis';
+import { createClient, defineScript } from 'redis';
+import { itemKey, itemsByViewsKey, itemsViewsKey } from '$services/keys';
+
 
 const client = createClient({
 	socket: {
 		host: process.env.REDIS_HOST,
 		port: parseInt(process.env.REDIS_PORT)
 	},
-	password: process.env.REDIS_PW
+	password: process.env.REDIS_PW,
+	// The "SCRIPT LOAD ..." implementation
+	scripts: {
+		// define script name and assign the script itself
+		addOneAndStore: defineScript({
+			NUMBER_OF_KEYS: 1,
+			SCRIPT: `
+				return redis.call('SET', KEYS[1], 1 + tonumber(ARGV[1]))
+				`,
+			transformArguments(key: string, value: number):string[] {
+				return [key, value.toString()];
+				// ['items:view', '55']  ==> to be transformed to ==> EVALSHA <ID> 1 'items:view' '55'
+			},
+			transformReply(reply: unknown): unknown { return reply; }
+		}),
+		incrementViews: defineScript({
+			NUMBER_OF_KEYS: 3,
+			SCRIPT: `
+					local itemsViewsKey = KEYS[1]
+					local itemKey = KEYS[2]
+					local itemsByViewsKey = KEYS[3]
+					local userId = ARGV[1]
+					local itemId = ARGV[2]
+					
+					local inserted = redis.call('PFADD', itemsViewsKey, userId)
+					
+					if inserted == 1 then
+						redis.call( 'HINCRBY', itemKey, 'views',1 )
+						redis.call( 'ZINCRBY', 'itemsByViewsKey', 1, itemId )
+					end
+			`,
+			transformArguments(itemId: string, userId: string):string[] {
+				return [
+					itemsViewsKey(itemId),  // items:views#asdf
+					itemKey(itemId),        // item#asdf
+					itemsByViewsKey(),      // items:views
+					itemId,                 // asdf
+					userId                  // u123
+				];
+				// EVALSHA <ID> 3 'items:views#asdf' 'item#asdf' 'items:views' 'asdf' 'u123'
+			},
+			transformReply():void { }
+		})
+	}
 });
+
+// The common test for the first implemented script:
+// client.on('connect', async () => {
+// 	await client.addOneAndStore('test:count', 5);
+// 	const temp = await client.get('test:count');
+// 	console.log('test the Lua scripting ', temp);
+// })
 
 client.on('error', (err) => console.error(err));
 client.connect();
